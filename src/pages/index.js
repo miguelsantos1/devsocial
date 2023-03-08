93ab1e88cee7101faed6980cf8afe3f13af3e9e9
Miguel de Oliveira Santos <miguel.santos6700@gmail.com>
2023-03-08 17:41:15 -0300

    Atualizando index

--- src/pages/index.js
+++ src/pages/index.js
@@ -59,14 +59,14 @@ export async function getServerSideProps(context) {
 
   const session = await getSession(context)
 
-  // if(!session) {
-  //   return {
-  //     redirect: {
-  //       destination: '/login',
-  //       permanent: false
-  //     }
-  //   }
-  // }
+  if(!session) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false
+      }
+    }
+  }
   
   return {
     props: {
